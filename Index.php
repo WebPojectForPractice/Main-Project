8316ad5d00fbb6a723b6fb76d7fcc9028ee515a1
WebPojectForPractice <148016480+WebPojectForPractice@users.noreply.github.com>
2023-10-16 01:13:45 +0200

    Update and rename Index.html to Index.php

---
+++ Index.php
@@ -0,0 +1,14 @@
+<?php include_once "./INC/Header.php";?>
+
+      </div>
+    </div>
+  </nav>
+  <div class="main">
+    <div class="white">
+        <h1> Something text to show off </h1>
+        <p class="mainP1"> This page is your greatest (and only) <span class="owner"> Blue! </span> Be amazed. </p>
+        <div class="spacer"></div>
+    </div>
+    
+  </div>
+<?php include_once "./INC/Footer.php";?>
